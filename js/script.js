b6060ca91fdeea5bade5b9d9f1d5e8a01092cb91
ketty <ketty.ammy@student.moringaschool.com>
2023-08-28 01:00:51 +0300

    form details

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const serviceItems = document.querySelectorAll('.what-we-do-item');
 
-// Add event listeners for mouse enter and leave events
 serviceItems.forEach(item => {
     item.addEventListener('mouseenter', () => {
         item.classList.add('hovered'); 
