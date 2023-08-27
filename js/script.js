13222df7e5e45f1ce2a5385d3995dd63b6299c62
ketty <ketty.ammy@student.moringaschool.com>
2023-08-28 01:02:23 +0300

    text box formatting

--- js/script.js
+++ js/script.js
@@ -9,3 +9,24 @@ serviceItems.forEach(item => {
         item.classList.remove('hovered'); 
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contact-form');
+
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the form from actually submitting
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+
+        
+        //console.log('Name:', name);
+        //console.log('Email:', email);
+        //console.log('Message:', message);
+        confirmation.style.display = 'block';
+
+        // Clear the form after submission (optional)
+        contactForm.reset();
+    });
+});
